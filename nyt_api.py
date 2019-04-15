890b47903304c067de6e60f60fc70cec18d9cb37
Jamie Spiel <jspiel@umich.edu>
2019-04-15 17:58:37 -0400

    started working

--- nyt_api.py
+++ nyt_api.py
@@ -1,39 +1,42 @@
 import requests
 import json
 import secrets
-#import api_info
+import nyt_info
 
-def makeRequestsUsingCache(url, params, headers, ident):
-	try:
-		cache_file1 = open('cache-nyt.json', 'r')
-		cache_contents1 = cache_file1.read()
-		CACHE_DICTION1 = json.loads(cache_contents1)
-		cache_file1.close()
-	except:
-		CACHE_DICTION1 = {}
-		response = requests.get("https://api.nytimes.com/svc/mostpopular/v2/viewed/1.json?api-key=JKiZea6whSW2E2AfOcJm1UgZHaCTrG2I")
-	if ident in CACHE_DICTION1:
-		return CACHE_DICTION1[ident]
-	else:
-		response = requests.get(url, params = params, headers = headers)
-		CACHE_DICTION1[ident] = response.text
-		dumpJSONCache = json.dumps(CACHE_DICTION1)
-		f1 = open('cache-nyt.json', 'w')
-		f1 = f1.write(dumpJSONCache)
-		f1.close()
-	return CACHE_DICTION1[ident]
+def makeRequestsUsingCache(url, ident): #params, headers, ident
+    ident = ident
+    try:
+        cache_file1 = open('cache-nyt.json', 'r')
+        cache_contents1 = cache_file1.read()
+        CACHE_DICTION1 = json.loads(cache_contents1)
+        cache_file1.close()
+    except:
+        CACHE_DICTION1 = {}
+    if ident in CACHE_DICTION1:
+        return CACHE_DICTION1[ident]
+    else:
+        response = requests.get(url) #params = params, headers = headers
+        CACHE_DICTION1[ident] = response.text
+        dumpJSONCache = json.dumps(CACHE_DICTION1)
+        f1 = open('cache-nyt.json', 'w')
+        f1 = f1.write(dumpJSONCache)
+        f1.close()
+    return CACHE_DICTION1[ident]
 
-def nytRequest(baseURL, params = {}):
-    req = requests.Request(method = 'GET', url = baseURL, params = sorted(params.items()))
-    prepped = req.prepare()
-    fullURL = prepped.url
-  
-    if fullURL not in CACHE_DICTION1:
-        response = requests.Session().send(prepped)
-        CACHE_DICTION1[fullURL] = response.text
-        
-        cache_file = open('cache-nyt.json', 'w')
-        cache_file.write(json.dumps(CACHE_DICTION1))
-        cache_file.close()
-    
-    return CACHE_DICTION1[fullURL]
+def nytRequest(ident):
+    # header = 
+    # params = {}
+    # params['limit'] = 
+    # params
+    base_url = 'https://api.nytimes.com/svc/mostpopular/v2/viewed/1.json?api-key='
+    my_key = nyt_info.api_key
+    url = base_url + my_key
+    response = makeRequestsUsingCache(url, ident) # params = params, headers = headers
+    #response = requests.get(url)
+    #data = json.loads(response)
+    # l_ = []
+    # for x in json.loads(response):
+    #     print(x)
+    return response
+
+print(nytRequest('trump'))
